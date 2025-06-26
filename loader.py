851b1b8e6b2518255e0289f096d1b0731b23ccbf
m1h1r-a <mihir.annambhotla@gmail.com>
2025-06-26 08:45:22 +0530

    calculate chunk id's

--- loader.py
+++ loader.py
@@ -1,9 +1,13 @@
 import langchain
 from langchain.document_loaders.pdf import PyPDFDirectoryLoader
 from langchain.schema.document import Document
+from langchain.vectorstores.chroma import Chroma
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_core import embeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 DATA_PATH = "/home/m1h1r/Documents/[2] dev/tvs-chatbot/data"
+CHROMA_PATH = "/home/m1h1r/Documents/[2] dev/tvs-chatbot/chroma"
 
 
 def load_documents():
@@ -21,12 +25,36 @@ def split_docs(documents: list[Document]):
     return text_splitter.split_documents(documents)
 
 
+def get_embedding_function():
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    return embeddings
+
+
+def calculate_chunk_ids(chunks):
+    chunk_index = 0
+    prev_id = None
+    for chunk in chunks:
+
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        cur_id = f"{source}:{page}"
+        # print(cur_id, "\n")
+
+        if cur_id == prev_id or prev_id == None:
+            final_id = f"{cur_id}:{chunk_index}"
+            print(f"{final_id}\n")
+            chunk_index += 1
+            prev_id = cur_id
+        else:
+            chunk_index = 0
+            final_id = f"{cur_id}:{chunk_index}"
+            print(f"{final_id}\n")
+            chunk_index += 1
+            prev_id = cur_id
+        chunk.metadata["id"] = final_id
+    return chunks
+
+
 docs = load_documents()
 chunks = split_docs(docs)
-
-print(len(docs))
-print("\n\n\n\n")
-print(docs[2])
-print("\n\n\n\n")
-print(len(chunks))
-print(chunks[2])
+chunks = calculate_chunk_ids(chunks)
